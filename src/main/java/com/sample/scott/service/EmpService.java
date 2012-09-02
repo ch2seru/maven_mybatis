43f6d6b502ef487df9cb3590b4c9f343d18bb723
lke <ch2seru@gmail.com>
2012-09-02 18:59:00 +0900

    네이밍정리

--- src/main/java/com/sample/scott/service/EmpService.java
+++ src/main/java/com/sample/scott/service/EmpService.java
@@ -1,6 +1,10 @@
 package com.sample.scott.service;
 
+import com.sample.scott.dao.IEmpDao;
 import com.sample.scott.domain.Emp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -9,12 +13,29 @@ import java.util.List;
  * User: lke
  * Date: 12. 3. 26
  */
-public interface EmpService {
+@Transactional
+@Service
+public class EmpService implements IEmpService {
+    @Autowired private IEmpDao empDao;
 
-    public List<Emp> getList();
-    public Emp get(int empno);
-    public void save(Emp emp);
-    public void modify(Emp emp);
-    public void remove(int empno);
 
+    public List<Emp> getList() {
+        return empDao.list();
+    }
+
+    public Emp get(int empno) {
+        return empDao.get(empno);
+    }
+
+    public void save(Emp emp) {
+        empDao.save(emp);
+    }
+
+    public void modify(Emp emp) {
+        empDao.update(emp);
+    }
+
+    public void remove(int empno) {
+        empDao.delete(empno);
+    }
 }
---
+++ src/main/java/com/sample/scott/service/IEmpService.java
@@ -0,0 +1,20 @@
+package com.sample.scott.service;
+
+import com.sample.scott.domain.Emp;
+
+import java.util.List;
+
+/**
+ * Subject :
+ * User: lke
+ * Date: 12. 3. 26
+ */
+public interface IEmpService {
+
+    public List<Emp> getList();
+    public Emp get(int empno);
+    public void save(Emp emp);
+    public void modify(Emp emp);
+    public void remove(int empno);
+
+}
